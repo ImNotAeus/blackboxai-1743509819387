a8adc7986e78b506f70ad2ea50ee5885e0019909
ImNotAeus <149932995+ImNotAeus@users.noreply.github.com>
2025-04-01 21:14:12 +0800

    Add pages/dashboard.php 1743513250274

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -26,6 +26,11 @@ $contacts = $conn->query("SELECT contact_name, contact_number FROM emergency_con
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Emergency System - Dashboard</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            darkMode: 'class'
+        }
+    </script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <script>
     function triggerEmergency() {
